1dd0fea3cff445a61326c22e924038a7b2176c7e
valentina-rojas <valentinarojas618@gmail.com>
2024-10-16 23:28:35 -0300

    zoom a medias

--- Assets/Scripts/Zoom.cs
+++ Assets/Scripts/Zoom.cs
@@ -1,28 +1,23 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Zoom : MonoBehaviour, IDragHandler
 {
-    public Camera magnifyingCamera; // La cámara secundaria que generará el zoom
-    private RectTransform rectTransform; // El RectTransform de la lupa
+    public RectTransform lupa; // El RectTransform de la lupa
 
-    void Start()
-    {
-        rectTransform = GetComponent<RectTransform>();
-    }
-
-    // Este método permite arrastrar la lupa por la pantalla
+    // Método que se llama cuando se arrastra el objeto
     public void OnDrag(PointerEventData eventData)
     {
-        // Mueve la lupa en la dirección del ratón
-        rectTransform.anchoredPosition += eventData.delta;
-    }
+        // Mueve la lupa a la posición del puntero del mouse durante el arrastre
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            lupa.parent as RectTransform, // El contenedor de la lupa
+            eventData.position,           // La posición actual del puntero
+            eventData.pressEventCamera,   // La cámara que interactúa con el UI
+            out pos
+        );
 
-    void Update()
-    {
-        // Convertir la posición de la lupa (en coordenadas de UI) a coordenadas de mundo
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        // Actualizar la posición de la cámara secundaria para que siga a la lupa
-        magnifyingCamera.transform.position = new Vector3(worldPosition.x, worldPosition.y, magnifyingCamera.transform.position.z);
+        lupa.anchoredPosition = pos; // Actualiza la posición de la lupa
     }
 }
